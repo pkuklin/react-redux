dfd4c862788dc2f40822322773af5f412364d6f7
pkuklin <kuklinpavlo@gmail.com>
2017-03-12 23:14:59 +0200

    API integration, render table of Courses

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,4 +1,4 @@
 /**
  * Created by pkuklin on 3/12/17.
  */
-export const CREATE_COURSE = 'CREATE_COURSE';
+export const LOAD_COURSES_SUCCESS = 'LOAD_COURSES_SUCCESS';
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,32 +1,16 @@
 /**
  * Created by pkuklin on 3/8/17.
  */
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
 
-        this.state = {
-            course: {title: ''}
-        };
-
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-
-    }
-
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
-
-    onClickSave() {
-        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index){
@@ -34,25 +18,20 @@ class CoursesPage extends React.Component {
     }
 
     render() {
+        const {courses} = this.props;
+
         return (
             <div>
                 <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input type="text"
-                       onChange={this.onTitleChange}
-                       value={this.state.course.title}/>
-                <input type="submit"
-                       value="Save"
-                       onClick={this.onClickSave}/>
+                <CourseList courses={courses}/>
             </div>
         );
     }
 }
 
 CoursesPage.propTypes = {
-    courses: React.PropTypes.array.isRequired,
-    actions: React.PropTypes.object.isRequired
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
